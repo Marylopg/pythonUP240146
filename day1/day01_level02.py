4ca5676b964d2f37fbd43d54a3b67d52fb8bd34f
UP240146 <up240146@alumnos.upa.edu.mx>
2025-06-11 21:59:20 -0600

    Actualización

---
+++ day1/day01_level02.py
@@ -0,0 +1,19 @@
+### Exercise: Level 2
+
+#1
+#Python 3.13.3
+
+#2
+print(3 + 4)     # 7
+print(3 - 4)     # -1
+print(3 * 4)     # 12
+print(3 % 4)     # 3
+print(3 / 4)     # 0.75
+print(3 ** 4)    # 81
+print(3 // 4)    # 0
+
+#3
+print('Mariana Veronica')
+print ('Lopes Gonzale')
+print ('Mexico')
+print('I am enjoying 30 days of python')
---
+++ day1/day01_level03.py
@@ -0,0 +1,34 @@
+### Exercise: Level 3
+
+#1
+
+# Number
+int_num = 10
+float_num = 3.14
+complex_num = 4 + 5j
+# String
+name = "Mariana"
+# Boolean
+is_happy = True
+# List
+languages = ["Python", "JavaScript", "C++"]
+# Tuple
+coordinates = (10.5, 20.8)
+# Set
+unique_numbers = {1, 2, 3}
+# Dictionary
+person = {
+    "name": "Mariana",
+    "country": "Mexico",
+    "age": 19
+}
+
+#2
+
+import math
+
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print(distance)  # 9.433981132056603
